fd9751cd8be6368d67dc611e93e715c57553f346
DeletedUser-0 <71786022+DeletedUser-0@users.noreply.github.com>
2021-09-18 21:01:19 +0100

    Update primary_points.js

--- js/primary_points.js
+++ js/primary_points.js
@@ -1,18 +1,48 @@
+function refresh() {
+    localStorage.player = JSON.stringify(player);
+    window.location.reload();
+};
+
 window.setInterval(function() {
     // calculate primary points
-    if (player.gupgrade5.bought == false) {
-        if (player.gupgrade6.bought == true) {
-            player.primary.earn = OmegaNum.pow(1.02, OmegaNum.log10(player.money.amount)).div(OmegaNum.pow(1.02, 308.255)).times(OmegaNum.log10(player.primary.amount).pow(1.5));
+    if (player.challenge4.completed == false) {
+        if (player.gupgrade5.bought == false) {
+            if (player.gupgrade6.bought == true) {
+                player.primary.earn = OmegaNum.pow(1.02, OmegaNum.log10(player.money.amount)).div(OmegaNum.pow(1.02, 308.255)).times(OmegaNum.log10(player.primary.amount).pow(1.5));
+            } else {
+                player.primary.earn = OmegaNum.pow(1.02, OmegaNum.log10(player.money.amount)).div(OmegaNum.pow(1.02, 308.255));
+            };
         } else {
-            player.primary.earn = OmegaNum.pow(1.02, OmegaNum.log10(player.money.amount)).div(OmegaNum.pow(1.02, 308.255));
+            if (player.gupgrade6.bought == true) {
+                if (player.gupgrade15.bought == true) {
+                    player.primary.earn = OmegaNum.pow(1.02, OmegaNum.log10(player.money.amount)).div(OmegaNum.pow(1.02, 308.255)).times(30).times(OmegaNum.log10(player.primary.amount).pow(1.5)).times(OmegaNum.pow(2, player.upgrade6.level));
+                } else {
+                    player.primary.earn = OmegaNum.pow(1.02, OmegaNum.log10(player.money.amount)).div(OmegaNum.pow(1.02, 308.255)).times(30).times(OmegaNum.log10(player.primary.amount).pow(1.5));
+                };
+            } else {
+                player.primary.earn = OmegaNum.pow(1.02, OmegaNum.log10(player.money.amount)).div(OmegaNum.pow(1.02, 308.255)).times(30);
+            };
         };
     } else {
-        if (player.gupgrade6.bought == true) {
-            player.primary.earn = OmegaNum.pow(1.02, OmegaNum.log10(player.money.amount)).div(OmegaNum.pow(1.02, 308.255)).times(30).times(OmegaNum.log10(player.primary.amount).pow(1.5));
+        if (player.gupgrade5.bought == false) {
+            if (player.gupgrade6.bought == true) {
+                player.primary.earn = OmegaNum.pow(1.02, OmegaNum.log10(player.money.amount)).div(OmegaNum.pow(1.02, 308.255)).times(OmegaNum.log10(player.primary.amount).pow(1.5)).times(15);
+            } else {
+                player.primary.earn = OmegaNum.pow(1.02, OmegaNum.log10(player.money.amount)).div(OmegaNum.pow(1.02, 308.255)).times(15);
+            };
         } else {
-            player.primary.earn = OmegaNum.pow(1.02, OmegaNum.log10(player.money.amount)).div(OmegaNum.pow(1.02, 308.255)).times(30);
+            if (player.gupgrade6.bought == true) {
+                if (player.gupgrade15.bought == true) {
+                    player.primary.earn = OmegaNum.pow(1.02, OmegaNum.log10(player.money.amount)).div(OmegaNum.pow(1.02, 308.255)).times(30).times(OmegaNum.log10(player.primary.amount).pow(1.5)).times(OmegaNum.pow(2, player.upgrade6.level)).times(15);
+                } else {
+                    player.primary.earn = OmegaNum.pow(1.02, OmegaNum.log10(player.money.amount)).div(OmegaNum.pow(1.02, 308.255)).times(30).times(OmegaNum.log10(player.primary.amount).pow(1.5)).times(15);
+                };
+            } else {
+                player.primary.earn = OmegaNum.pow(1.02, OmegaNum.log10(player.money.amount)).div(OmegaNum.pow(1.02, 308.255)).times(30).times(15);
+            };
         };
-    };
+
+    }
     // fix NaN errors
     if (isNaN(player.gen1.total)) {
         player.gen1.total = player.gen1.total.array[0];
@@ -31,24 +61,63 @@ window.setInterval(function() {
         upgrade2();
         upgrade3();
         upgrade4();
+        upgrade5();
+        upgrade6();
     };
+    if (player.gupgrade10.bought == true) {
+        player.gen1.cost = OmegaNum.pow(player.gen1.costupdate, OmegaNum.div(1, OmegaNum.pow(OmegaNum.log10(player.primary.amount), 0.2)));
+        player.gen2.cost = OmegaNum.pow(player.gen2.costupdate, OmegaNum.div(1, OmegaNum.pow(OmegaNum.log10(player.primary.amount), 0.2)));
+        player.gen3.cost = OmegaNum.pow(player.gen3.costupdate, OmegaNum.div(1, OmegaNum.pow(OmegaNum.log10(player.primary.amount), 0.2)));
+    } else {
+        player.gen1.costupdate = player.gen1.cost;
+        player.gen2.costupdate = player.gen2.cost;
+        player.gen3.costupdate = player.gen3.cost;
+    };
+    if (player.gupgrade12.bought == false) {
+        player.gen3.amount = player.gen3.level;
+    }
 }, 0);
 
+// runs 50 times per second
+window.setInterval(function() {
+    player.gen3.before = player.gen3.amount;
+    if (player.gupgrade12.bought == true) {
+        if (player.gupgrade17.bought == false) {
+            player.gen3.amount = OmegaNum.add(player.gen3.amount, OmegaNum.div(OmegaNum.pow(OmegaNum.log10(player.gen3.amount), OmegaNum.add(3, OmegaNum.add(OmegaNum.log10(player.gen3.amount).div(2.5), 1))), 50));
+        } else {
+            player.gen3.amount = OmegaNum.add(player.gen3.amount, OmegaNum.pow(OmegaNum.log10(player.gen3.amount), OmegaNum.add(3, OmegaNum.add(OmegaNum.log10(player.gen3.amount).div(2.3), 1))));
+        };
+    };
+}, 20);
+
 function addprimary() {
-    if (OmegaNum.cmp(player.money.amount, new OmegaNum("1.7932e308")) >= 0) {
-        player.primary.amount = OmegaNum.add(player.primary.amount, player.primary.earn);
-        player.primary.reset += 1;
-        if (player.gupgrade7.bought == false) {
-            player.money.amount = 0;
-            player.money.base = 1;
-            player.money.second = 1;
-            resetprimary();
-            resetprimary();
+    if (player.gupgrade7.bought == false) {
+        if (OmegaNum.cmp(player.money.amount, new OmegaNum("1.7932e308")) >= 0) {
+            player.primary.amount = OmegaNum.add(player.primary.amount, player.primary.earn);
+            player.primary.reset += 1;
+            if (player.primary.reset >= 1) {
+                document.getElementById("gupgrade1").style.display = "block-inline";
+            };
+            if (player.primary.reset == 1) {
+                refresh()  
+            };
+            if (player.gupgrade7.bought == false) {
+                player.money.amount = 0;
+                player.money.base = 1;
+                player.money.second = 1;
+                player.money.maxbase = 1;
+                player.money.max = 1;
+                resetprimary();
+                resetprimary();
+            };
         };
     };
 };
 
-function resetprimary() {
+function resetuniverses() {
+    player.money.max = 1;
+    player.money.maxbase = 1;
+    player.money.second = 1;
     player.money.base = 1;
     player.money.amount = 0;
     player.upgrade1.cost = 10;
@@ -56,52 +125,116 @@ function resetprimary() {
     player.upgrade1.level = 0;
     player.auto.requires = 1000;
     player.auto.effect = 3;
+    player.auto.level = 0;
     player.upgrade2.level = 0;
-    player.upgrade2.cost = 1e12;
+    player.upgrade2.cost = 1e15;
     player.upgrade2.effect = 4;
-    player.upgrade3.cost = 1e51;
+    player.upgrade3.cost = 1e79;
     player.upgrade3.effect = 1;
     player.upgrade3.level = 0;
-    player.upgrade4.cost = 1e109;
-    player.upgrade4.level = 0;
+    player.upgrade4.cost = 1e113;
     player.upgrade4.effect = 1;
+    player.upgrade4.level = 0;
+};
+
+function resetprimary() {
+    player.money.max = 1;
+    player.money.maxbase = 1;
+    player.money.second = 1;
+    player.money.base = 1;
+    player.money.amount = 0;
+    player.upgrade1.cost = 10;
+    player.upgrade1.effect = 2;
+    player.upgrade1.level = 0;
+    player.auto.requires = 1000;
+    player.auto.effect = 3;
+    player.auto.level = 0;
+    player.upgrade2.level = 0;
+    player.upgrade2.cost = 1e15;
+    player.upgrade2.effect = 4;
+    player.upgrade3.cost = 1e79;
+    player.upgrade3.effect = 1;
+    player.upgrade3.level = 0;
+    player.upgrade4.cost = 1e113;
+    player.upgrade4.effect = 1;
+    player.upgrade4.level = 0;
+    player.universes.amount = 0;
+    player.universes.requires = 1e60;
 };
 
 function gen1() {
-    if (OmegaNum.cmp(player.primary.amount, player.gen1.cost) >= 0) {
+    while (OmegaNum.cmp(player.primary.amount, player.gen1.cost) >= 0) {
         player.primary.amount = OmegaNum.sub(player.primary.amount, player.gen1.cost);
-        if (OmegaNum.cmp(player.gen1.level, 15) <= 0) {
-            player.gen1.cost = 1;
+        if (OmegaNum.cmp(player.gen1.level, 1) <= 0) {
+            player.gen1.costupdate = 1;
         } else {
-            player.gen1.cost = OmegaNum.mul(player.gen1.cost, 1.15).pow(1.04);
-        }
+            if (player.gupgrade10.bought == true) {
+                if (OmegaNum.cmp(player.gen1.level, 90) < 0) {
+                    player.gen1.costupdate = OmegaNum.mul(player.gen1.costupdate, 1.15).pow(1.04);
+                } else {
+                    player.gen1.costupdate = OmegaNum.pow(player.gen1.costupdate, 1.02).times(1.15);
+                };
+            } else {
+                if (OmegaNum.cmp(player.gen1.level, 90) < 0) {
+                    player.gen1.cost = OmegaNum.mul(player.gen1.cost, 1.15).pow(1.04);
+                } else {
+                    player.gen1.cost = OmegaNum.pow(player.gen1.cost, 1.02).times(1.15);
+                };
+            }
+        };
         player.gen1.level += 1;
         player.gen1.total = OmegaNum.add(player.gen1.total, 1);
     };
 };
 
 function gen2() {
-    if (OmegaNum.cmp(player.primary.amount, player.gen2.cost) >= 0) {
+    while (OmegaNum.cmp(player.primary.amount, player.gen2.cost) >= 0) {
         player.primary.amount = OmegaNum.sub(player.primary.amount, player.gen2.cost);
-        player.gen2.cost = OmegaNum.pow(player.gen2.cost, 1.061).times(1.25);
+        if (player.gupgrade10.bought == true) {
+            if (OmegaNum.cmp(player.gen1.level, 90) < 0) {
+                player.gen2.costupdate = OmegaNum.pow(player.gen2.costupdate, 1.061).times(1.25);
+            } else {
+                player.gen2.costupdate = OmegaNum.pow(player.gen2.costupdate, 1.03);
+            };
+        } else {
+            if (OmegaNum.cmp(player.gen1.level, 90) < 0) {
+                player.gen2.cost = OmegaNum.pow(player.gen2.cost, 1.061).times(1.25);
+            } else {
+                player.gen2.cost = OmegaNum.pow(player.gen2.cost, 1.03);
+            };
+        };
         player.gen2.level += 1;
         player.gen2.total = OmegaNum.add(player.gen2.total, 1);
     };
 };
 
 function gen3() {
-    if (OmegaNum.cmp(player.primary.amount, player.gen3.cost) >= 0) {
-        player.primary.amount = OmegaNum.sub(player.primary.amount, player.gen3.cost);
-        player.gen3.cost = Math.floor(OmegaNum.pow(player.gen3.cost, 1.082));
+    while (OmegaNum.cmp(player.primary.amount, player.gen3.cost) >= 0) {
         player.gen3.level += 1;
         player.gen3.total = OmegaNum.add(player.gen3.total, 1);
+        player.primary.amount = OmegaNum.sub(player.primary.amount, player.gen3.cost);
+        if (player.gupgrade10.bought == true) {
+            if (OmegaNum.cmp(player.gen1.level, 90) < 0) {
+                player.gen3.costupdate = Math.floor(OmegaNum.pow(player.gen3.costupdate, 1.082));
+            } else {
+                player.gen3.costupdate = Math.floor(OmegaNum.pow(player.gen3.costupdate, 1.041));
+            };
+        } else {
+            if (OmegaNum.cmp(player.gen1.level, 90) < 0) {
+                player.gen3.cost = Math.floor(OmegaNum.pow(player.gen3.cost, 1.082));
+            } else {
+                player.gen3.cost = Math.floor(OmegaNum.pow(player.gen3.cost, 1.041));
+            };
+        };
     };
 };
 
 function gupgrade1() {
-    if (OmegaNum.cmp(player.primary.amount, 100) >= 0) {
-        player.primary.amount = OmegaNum.sub(player.primary.amount, 100);
+    if (OmegaNum.cmp(player.primary.amount, 10) >= 0) {
+        player.primary.amount = OmegaNum.sub(player.primary.amount, 10);
         player.gupgrade1.bought = true;
+        document.getElementById("gupgrade2").style.display = "block-inline";
+        refresh();
     };
 };
 
@@ -109,56 +242,153 @@ function gupgrade2() {
     if (OmegaNum.cmp(player.primary.amount, 250) >= 0) {
         player.primary.amount = OmegaNum.sub(player.primary.amount, 250);
         player.gupgrade2.bought = true;
+        document.getElementById("gupgrade3").style.display = "block-inline";
+        refresh();
     };
 };
 
 function gupgrade3() {
-    if (OmegaNum.cmp(player.primary.amount, 2000) >= 0) {
-        player.primary.amount = OmegaNum.sub(player.primary.amount, 2000);
+    if (OmegaNum.cmp(player.primary.amount, 1e4) >= 0) {
+        player.primary.amount = OmegaNum.sub(player.primary.amount, 1e4);
         player.gupgrade3.bought = true;
+        document.getElementById("gupgrade4").style.display = "block-inline";
+        refresh();
     };
 };
 
 function gupgrade4() {
-    if (OmegaNum.cmp(player.primary.amount, 10000) >= 0) {
-        player.primary.amount = OmegaNum.sub(player.primary.amount, 10000);
+    if (OmegaNum.cmp(player.primary.amount, 1e6) >= 0) {
+        player.primary.amount = OmegaNum.sub(player.primary.amount, 1e6);
         player.gupgrade4.bought = true;
+        document.getElementById("gupgrade5").style.display = "block-inline";
+        refresh();
     };
 };
 
 function gupgrade5() {
-    if (OmegaNum.cmp(player.primary.amount, 250000) >= 0) {
-        player.primary.amount = OmegaNum.sub(player.primary.amount, 250000);
+    if (OmegaNum.cmp(player.primary.amount, 1e7) >= 0) {
+        player.primary.amount = OmegaNum.sub(player.primary.amount, 1e7);
         player.gupgrade5.bought = true;
+        document.getElementById("gupgrade6").style.display = "block-inline";
+        refresh();
     };
 };
 
 function gupgrade6() {
-    if (OmegaNum.cmp(player.primary.amount, 2.5e7) >= 0) {
-        player.primary.amount = OmegaNum.sub(player.primary.amount, 2.5e7);
-        player.gupgrade6.bought = true;
+    if (OmegaNum.cmp(player.primary.amount, 2e6) >= 0) {
+        if (player.gupgrade6.bought == false) {
+            player.primary.amount = OmegaNum.sub(player.primary.amount, 2e6);
+            player.gupgrade6.bought = true;
+            document.getElementById("gupgrade7").style.display = "block-inline";
+            refresh();
+        };
     };
 };
 
 function gupgrade7() {
-    if (OmegaNum.cmp(player.primary.amount, 2e10) >= 0) {
-        player.primary.amount = OmegaNum.sub(player.primary.amount, 2e10);
+    if (OmegaNum.cmp(player.primary.amount, 1e8) >= 0) {
+        player.primary.amount = OmegaNum.sub(player.primary.amount, 1e8);
         player.gupgrade7.bought = true;
+        document.getElementById("gupgrade8").style.display = "block-inline";
+        refresh();
     };
 };
 
 function gupgrade8() {
-    if (OmegaNum.cmp(player.primary.amount, 1e12) >= 0) {
-        player.primary.amount = OmegaNum.sub(player.primary.amount, 1e12);
+    if (OmegaNum.cmp(player.primary.amount, 3e8) >= 0) {
+        player.primary.amount = OmegaNum.sub(player.primary.amount, 3e8);
         player.gupgrade8.bought = true;
-        player.money.second = OmegaNum.pow(player.money.second, 1.05);
+        player.money.base = OmegaNum.pow(player.money.base, 1.05);
+        document.getElementById("gupgrade9").style.display = "block-inline";
+        refresh();
     };
 };
 
 function gupgrade9() {
-    if (OmegaNum.cmp(player.primary.amount, 2.5e14) >= 0) {
-        player.primary.amount = OmegaNum.sub(player.primary.amount, 2.5e14);
+    if (OmegaNum.cmp(player.primary.amount, 1e10) >= 0) {
+        player.primary.amount = OmegaNum.sub(player.primary.amount, 1e10);
         player.gupgrade9.bought = true;
-        player.upgrade1.cost = OmegaNum.pow(player.upgrade1.cost, OmegaNum.pow(OmegaNum.div(0.95, 0.96), player.upgrade3.level));
+        player.upgrade1.cost = OmegaNum.pow(player.upgrade1.cost, OmegaNum.pow(OmegaNum.div(0.94, 0.96), player.upgrade3.level));
+        document.getElementById("gupgrade10").style.display = "block-inline";
+        refresh();
+    };
+};
+
+function gupgrade10() {
+    if (OmegaNum.cmp(player.primary.amount, 1e10) >= 0) {
+        player.primary.amount = OmegaNum.sub(player.primary.amount, 1e10);
+        player.gupgrade10.bought = true;
+        document.getElementById("gupgrade11").style.display = "block-inline";
+        refresh();
+    };
+};
+
+function gupgrade11() {
+    if (OmegaNum.cmp(player.primary.amount, 1e11) >= 0) {
+        player.primary.amount = OmegaNum.sub(player.primary.amount, 1e11);
+        player.gupgrade11.bought = true;
+        document.getElementById("gupgrade12").style.display = "block-inline";
+        refresh();
+    };
+};
+
+function gupgrade12() {
+    if (OmegaNum.cmp(player.primary.amount, 1e13) >= 0) {
+        player.primary.amount = OmegaNum.sub(player.primary.amount, 1e13);
+        player.gupgrade12.bought = true;
+        document.getElementById("gupgrade13").style.display = "block-inline";
+        refresh();
+    };
+};
+
+function gupgrade13() {
+    if (OmegaNum.cmp(player.primary.amount, 1e14) >= 0) {
+        player.primary.amount = OmegaNum.sub(player.primary.amount, 1e14);
+        player.gupgrade13.bought = true;
+        document.getElementById("gupgrade14").style.display = "block-inline";
+        refresh();
+    };
+};
+
+function gupgrade14() {
+    if (OmegaNum.cmp(player.primary.amount, 1e16) >= 0) {
+        player.primary.amount = OmegaNum.sub(player.primary.amount, 1e16);
+        player.gupgrade14.bought = true;
+        document.getElementById("gupgrade15").style.display = "block-inline";
+        refresh();
+    };
+};
+
+function gupgrade15() {
+    if (OmegaNum.cmp(player.primary.amount, 1e17) >= 0) {
+        player.primary.amount = OmegaNum.sub(player.primary.amount, 1e17);
+        player.gupgrade15.bought = true;
+        document.getElementById("gupgrade16").style.display = "block-inline";
+        refresh();
+    };
+};
+
+function gupgrade16() {
+    if (OmegaNum.cmp(player.primary.amount, 1e25) >= 0) {
+        player.primary.amount = OmegaNum.sub(player.primary.amount, 1e25);
+        player.gupgrade16.bought = true;
+        document.getElementById("gupgrade17").style.display = "block-inline";
+        refresh();
+    };
+};
+
+function gupgrade17() {
+    if (OmegaNum.cmp(player.primary.amount, 1e27) >= 0) {
+        player.primary.amount = OmegaNum.sub(player.primary.amount, 1e27);
+        player.gupgrade17.bought = true;
+        document.getElementById("gupgrade18").style.display = "block-inline";
+        refresh();
+    };
+};
+
+function gupgrade18() {
+    if (OmegaNum.cmp(player.primary.amount, 1e30) >= 0) {
+        player.primary.amount = OmegaNum.sub(player.primary.amount, 1e30);
+        player.gupgrade18.bought = true;
     };
 };
